79c50cbc2103210b6bd381cad7ebf1e22716ad1e
iliyan jeliazkov <iliyan@acm.org>
2015-12-28 12:38:13 -0600

    Adding a sample Protractor-based test, adopted from browserstack demos

---
+++ config.js
@@ -0,0 +1,20 @@
+exports.config = {
+  seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+  specs: ['spec.js'],
+
+  capabilities: {
+    browserName: 'Safari',
+    browser_version: '8.0',
+    os: 'OS X',
+    os_version: 'Yosemite',
+    resolution: '1024x768',
+
+    build: 'Sample Protractor Tests',
+    name: 'Sample Live Protractor Tests',
+
+    'browserstack.user': 'browserstack512',
+    'browserstack.key': 'XKDMu5yXdzAYvGT1eRqw'
+    //'browserstack.user': process.env.BROWSERSTACK_USERNAME,
+    //'browserstack.key': process.env.BROWSERSTACK_KEY
+  }
+};
